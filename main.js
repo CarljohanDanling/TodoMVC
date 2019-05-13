9c98d244b84e0a9a1399ac74da65ebc988e0143e
Carljohan Danling <carljohan.danling@gmail.com>
2019-05-13 18:32:29 +0200

    Adds a function to show the "select all" arrow or not. Adds functionality to deletion of activity. Now the corresponding activity is deleted from the array.

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 // sätt alla eventlisteners i början av programmet - "Jakob Kallin"
 
+const selectArrow = document.querySelector("#down-arrow");
+selectArrow.remove();
+
 const activity = document.querySelector("#activity");
 activity.remove();
 
-//let activities = [];
-let activityTexts = [];
+let activities = [];
+let activityIdCounter = 0;
 
 const form = document.querySelector("form");
 form.onsubmit = event => {
@@ -13,24 +16,36 @@ form.onsubmit = event => {
     const userInput = document.querySelector("#todo-input");
     const text = document.createTextNode(userInput.value);
 
-    activityTexts.push(text);
     renderActivity(text);
+    activityIdCounter += 1;
+
+    renderSelectAllArrow();
 
     form.reset();
-    /*
-    document.querySelector("[id=" + clone.id + "]")
-        .querySelector("P")
-        .appendChild(text);
-    */
+}
+
+function renderSelectAllArrow() {
+    if (activities.length === 1) {
+        document.querySelector("#select-all")
+            .appendChild(selectArrow);
+    } else if (activities.length > 1) {
+        return;
+    } else {
+        selectArrow.remove();
+    }
 }
 
 function renderActivity(text) {
     let clone = activity.cloneNode(true);
+    clone.id = clone.id + activityIdCounter;
+
     document.querySelector("#activity-section")
         .appendChild(clone);
 
     clone.querySelector("P")
         .appendChild(text);
+
+    activities.push(clone);
 
     clone.querySelector(".removal-sign")
         .addEventListener("click", () => {
@@ -40,4 +55,6 @@ function renderActivity(text) {
 
 function removeActivity(clone) {
     clone.remove();
+    activities = activities.filter(a => a.id !== clone.id);
+    renderSelectAllArrow();
 }
